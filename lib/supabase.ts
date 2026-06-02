6ee7718c9c0b2f64db25187f75cece29997a8920
zyaddaif <zyaddaifb@gmail.com>
2026-06-02 05:45:41 +0300

    v4

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,19 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+let supabaseClient: SupabaseClient;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = new Proxy({} as SupabaseClient, {
+  get(_, prop) {
+    if (!supabaseClient) {
+      const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+      const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+      if (!supabaseUrl || !supabaseAnonKey) {
+        throw new Error(
+          'Missing NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY'
+        );
+      }
+      supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+    }
+    return Reflect.get(supabaseClient, prop);
+  },
+});
